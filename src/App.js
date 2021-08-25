0afa5c8d94b5abf26ed07cb6480f8b62eacb3633
AKSHYA1997 <akshyaravishankar@hmail.com>
2021-08-26 02:21:54 +0530

    :sparkle: added task component and react icon package :sparkle: added delete function of tasks

--- src/App.js
+++ src/App.js
@@ -24,10 +24,19 @@ function App() {
     },
   ]);
 
+  const deleteTask = (id) => {
+    console.log("delete", id);
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        "No Tasks To Show!"
+      )}
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Task from "./Task";
 
-function Tasks({ tasks }) {
+function Tasks({ tasks, onDelete }) {
   return (
     <>
       {tasks.map((task) => (
-        <h3 key={task.id}>{task.text}</h3>
+        <Task key={task.id} task={task} onDelete={onDelete} />
       ))}
     </>
   );
