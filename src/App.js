55af1a267a6c23ef03c9ee422816abc135e74b6b
AKSHYA1997 <akshyaravishankar@hmail.com>
2021-08-26 02:36:27 +0530

    :sparkles: added toggle reminder function for tasks

--- src/App.js
+++ src/App.js
@@ -29,11 +29,20 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
+  const toggleReminder = (id) => {
+    console.log("toggle", id);
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className="container">
       <Header />
       {tasks.length > 0 ? (
-        <Tasks tasks={tasks} onDelete={deleteTask} />
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
         "No Tasks To Show!"
       )}
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { FaTimes } from "react-icons/fa";
 
-function Task({ task, onDelete }) {
+function Task({ task, onDelete, onToggle }) {
   return (
-    <div className="task">
+    <div
+      className={`task ${task.reminder ? "reminder" : ""}`}
+      onDoubleClick={() => onToggle(task.id)}
+    >
       <h3>
         {task.text}
         <FaTimes
